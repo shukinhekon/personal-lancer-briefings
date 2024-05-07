21dfc33b677407b60a46221a9f67c51bde0ad9c8
Eder Wilson Sousa da Luz Filho <ederwslf@gmail.com>
2024-05-07 18:31:24 -0300

    Testing Event List

--- src/App.vue
+++ src/App.vue
@@ -96,8 +96,13 @@ export default {
         {
           "slug": "001",
           "name": "Bug-Hunt",
-          "status": "start"
+          "status": "complete"
         },
+		{
+		  "slug": "002",
+          "name": "Vigilant Gaze",
+          "status": "start"
+		},
       ],
       "pilots": [
         {
